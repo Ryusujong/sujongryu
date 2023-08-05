de6e9113a3022e92efc61be1693a8b9e5c9d4be0
Ryusujong <140146044+Ryusujong@users.noreply.github.com>
2023-08-05 22:12:40 +0900

    Add files via upload

---
+++ tb_traffic_fsm.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+
+module tb_traffic_fsm();
+
+
+logic clk;
+logic rst_n;
+
+traffic_fsm north, south, east, west; 
+
+traffic_fsm dut(
+    .clk(clk)
+   ,.rst_n(rst_n)
+   ,.north(north)
+   ,.south(south)
+   ,.east(east)
+   ,.west(west)
+);
+
+ 
+initial begin
+   clk    = 0;
+   rst_n    = 1;
+
+   #20
+   rst_n    = 0;
+
+   #20
+   rst_n    = 1;
+end
+
+
+always begin
+   #5
+   clk = ~clk;
+end
+
+
+endmodule
---
+++ traffic_fsm.sv
@@ -0,0 +1,131 @@
+// enum ->열거형 변수 두비트 구성 총 네 개
+typedef enum logic [1:0] {
+   GREEN,
+   YELLOW,
+   RED,
+   LEFT
+} traffic_fsm;
+
+//매크로 상수 사용
+module traffic_fsm (
+   input clk, rst_n,
+   output traffic_fsm north, south, east, west, left
+);
+
+   localparam DURATION_GREEN = 40;
+   localparam DURATION_YELLOW = 5;
+   localparam DURATION_left = 20;
+
+//열거형 변수 사용 3비트 총 8개
+   enum logic [2:0] {
+      NS_GREEN,
+      NS_YELLOW,
+      NS_LEFT,
+      NS_YELLOW2,
+      EW_LEFT,
+      EW_YELLOW,
+      EW_GREEN,
+      EW_YELLOW2
+   } state, next_state;
+
+//지속시간을 알려주는 변수 선언 
+   int counter;
+
+
+// 리셋들어오면 남북초록 아니면 다음상태 넘어가기
+   always_ff @(posedge clk or negedge rst_n) begin
+      if(~rst_n)       state    <= NS_GREEN;
+      else          state    <= next_state;
+   end
+
+always_comb begin
+    next_state = state;  
+    case (state)
+            NS_GREEN: 
+                if (counter == DURATION_GREEN) 
+                    next_state = NS_YELLOW;
+            NS_YELLOW: 
+                if (counter == DURATION_YELLOW) 
+                    next_state = EW_LEFT;
+            EW_LEFT: 
+                if (counter == DURATION_left) 
+                    next_state = EW_YELLOW;
+            EW_YELLOW: 
+                if (counter == DURATION_YELLOW) 
+                    next_state = EW_GREEN;
+            EW_GREEN: 
+                if (counter == DURATION_GREEN) 
+                    next_state = EW_YELLOW2;
+            EW_YELLOW2: 
+                if (counter == DURATION_YELLOW) 
+                    next_state = NS_LEFT;
+            NS_LEFT: 
+                if (counter == DURATION_left) 
+                    next_state = NS_YELLOW2;
+            NS_YELLOW2: 
+                if (counter == DURATION_YELLOW) 
+                    next_state = NS_GREEN;
+            default: 
+                next_state = NS_GREEN;            
+    endcase
+end
+
+always_ff @(posedge clk or negedge rst_n)
+    if (!rst_n)begin   
+        north <= RED;
+        south <= RED;
+        east  <= RED;
+        west  <= RED;
+    
+    if(state  != next_state) begin
+        counter <= 0;
+    end
+    else begin    
+        counter <= counter + 1;
+    end
+    
+    case(next_state)
+            NS_GREEN: 
+                begin 
+                north <= GREEN;
+                south <= GREEN;
+                end
+            NS_YELLOW: 
+                begin
+                north <= YELLOW;
+                south <= YELLOW;
+                end
+            EW_LEFT: 
+                begin
+                east <= LEFT;
+                west <= LEFT;
+                end
+            EW_YELLOW: 
+                begin
+                east <= YELLOW;
+                west <= YELLOW;
+                end
+            EW_GREEN: 
+                begin
+                east <= GREEN;
+                west <= GREEN; 
+                end
+            EW_YELLOW2: 
+                begin 
+                east <= YELLOW;
+                west <= YELLOW;
+                end
+            NS_LEFT: 
+                begin
+                north <= LEFT;
+                south <= LEFT;
+                end
+            NS_YELLOW2: 
+                begin
+                north <= YELLOW;
+                south <= YELLOW;
+             end  
+    endcase 
+end    
+     
+endmodule
